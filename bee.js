b92e81e0a80aa0b8ff7bb30489c863d62abc34d9
josephinetsai <67291834+josephinetsai@users.noreply.github.com>
2022-02-14 12:34:09 -0800

    Update bee.js

--- bee.js
+++ bee.js
@@ -3,3 +3,25 @@ var posX;
 var posY;
 var size;
 var direction; 
+
+
+function move()
+{
+  if (this.direction==0)
+  {
+    posX ++; 
+  }
+  if (this.direction==2)
+  {
+    posX --; 
+  }
+  if (this.direction==3)
+  {
+    posY --; 
+  }
+  if (this.direction==1)
+  {
+    posY ++; 
+  }
+  
+}
