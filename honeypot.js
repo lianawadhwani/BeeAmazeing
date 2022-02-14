81d3537c5ad40ce1c92f9c8f128f4ba81f8aaa24
rhea <rhea0866@gmail.com>
2022-02-14 12:48:50 -0800

    Update honeypot.js

--- honeypot.js
+++ honeypot.js
@@ -1,1 +1,46 @@
 
+
+let onScreenTime;
+let posX;
+let posY;
+
+function setOnScreenTime(){
+  onScreenTime = Math.floor(Math.random() * 20) + 5;
+}
+
+function getOnScreenTime(){
+  return onScreenTime;
+}
+
+
+function setPosX(){
+
+}
+
+function getPosX(){
+  return posX;
+}
+function setPosY(){
+
+}
+
+function getPosX(){
+  return posY;
+}
+
+
+
+function collect(gameTimer){
+  //change style of honeypot element to be invisible
+  gameTimer += 5;
+}
+
+function disappear(){
+  if(time == 0){
+    //change style of honeypot element to be invisible
+  }
+}
+
+function setTime(){
+
+}
