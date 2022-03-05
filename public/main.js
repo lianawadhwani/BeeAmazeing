4d0020a84564b16e867a903b37d5bba43fcf6d66
rhea <rhea0866@gmail.com>
2022-03-04 22:57:31 -0800

    Update main.js

--- public/main.js
+++ public/main.js
@@ -24,16 +24,20 @@ test.setVisible(true);
 
 ctx.clearRect(0,0, canvas.width, canvas.height);
 var myImg = document.getElementById("img");
-var bee= document. getElementById("bee");
+var bee = document. getElementById("bee");
 ctx.drawImage(bee,30, 150, 50, 50)
 ctx.drawImage(myImg, 0, 0, 390, 390);
 ctx.drawImage(image, 350, 150,50, 50);
 if(watch.state == "running" && watch.value != 100){
+  //could possibly change if statement to rely on honeypots visible variable
   test.drawPot();
-  console.log(watch.value);
-  //test.setVisible(false);
-  console.log(test.getVisible());
+  console.log("pot is on screen");
 
+  //console.log(test.getVisible());
+}
+else{
+  console.log("pot is off screen");
+  test.setVisible(false);
 }
 
 
