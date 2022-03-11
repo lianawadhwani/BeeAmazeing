9272396deff4ec3ded26da1f9ac1bc613ff25ce2
coliny125 <colinyuan3333@gmail.com>
2022-03-11 09:16:09 -0800

    last changes but it works

--- public/main.js
+++ public/main.js
@@ -18,7 +18,7 @@ var pot = new Honeypot(170, 150,bee1);
 pot.setOnScreenTime(30);
 
 let level =new Levels();
-level.changeLevel(5);
+level.changeLevel(1);
 
 
 document.addEventListener('keydown',(event)=>{
